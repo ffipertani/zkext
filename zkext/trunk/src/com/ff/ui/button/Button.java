fc6d8a0a6dedfb6d15dc2bd2d23680d2977e0f89
francesco.fipertani@gmail.com <francesco.fipertani@gmail.com@0e297f28-f901-9d45-cf6f-4f06dcf8dffe>
2011-09-26 08:38:50 +0000

    Corretta dipendenza

--- zkext/trunk/src/com/ff/ui/button/Button.java
+++ zkext/trunk/src/com/ff/ui/button/Button.java
@@ -1,9 +1,8 @@
 package com.ff.ui.button;
 
-import org.zkoss.zkext.ui.button.events.ClickEvent;
-
 import com.ff.Component;
 import com.ff.annotation.Property;
+import com.ff.event.ClickEvent;
 
 public class Button extends Component{
 	static {
